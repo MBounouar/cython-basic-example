6d97173d12183e2e95849d6a70b426bc7194ad0e
MBounouar <MBounouar@users.noreply.github.com>
2022-01-19 21:15:06 +0100

    cleanup

--- hello.pyx
+++ hello.pyx
@@ -6,11 +6,8 @@ from libc.stdio cimport printf
 
 cdef extern from "spam.h":
     void order_spam()
-    print(a,b)
 
 def test_hello():
-    cdef int num_threads
-    
     printf("Hello ")
     printf("World \n")
     return order_spam()
